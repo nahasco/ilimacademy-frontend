723047e0ced83265fcf015774a6f42ec12171edf
Mohammad Alnahhas <alnahhas55@gmail.com>
2022-08-14 00:11:48 +0300

    fix file name

--- src/pages/practice/math.js
+++ src/pages/practice/math.js
@@ -1,6 +1,6 @@
 import Layout from "../../components/layout/Layout";
 import PracticeLayout from "../../components/Layout/PracticeLayout";
-import Practice from "../../components/modules/Practice.mjs";
+import Practice from "../../components/modules/Practice.js";
 import Sections from "../../components/modules/Sections";
 
 export default function Math() {
