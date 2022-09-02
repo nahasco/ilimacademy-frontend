1e548b4ee7260344e0cbca8d90b538c2bce2a4cc
nahasco <alnahhas55@gmail.com>
2022-09-03 02:43:07 +0300

    edit logo tag

--- src/pages/login.js
+++ src/pages/login.js
@@ -56,11 +56,7 @@ export default function LoginPage() {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="/Ilim.svg"
-              alt="Workflow"
-            />
+            <div>Logo</div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={onSubmit}>
