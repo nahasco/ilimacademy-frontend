29be2030d3b2b77bd51f166dd5e72d889c8884ba
nahasco <alnahhas55@gmail.com>
2022-09-13 23:57:19 +0300

    added notes feature

--- src/components/modules/Sections.js
+++ src/components/modules/Sections.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Notes_icon from '../../icons/notes_icon'
 import useData from '../../stores/useData'
 import Skill_level from '../Skill_level'
@@ -6,6 +6,8 @@ import Link from "next/link"
 import practiceExercise from "../../utils/practiceExercise"
 import useStore from '../../stores/userStore'
 import FullPageLoader from '../FullPageLoader'
+import Popup from './Popup'
+import { API_URL } from '../../config'
 
 export default function Sections({subject}) {
     const data = useData((state) => state.data)
@@ -68,6 +70,15 @@ function Topic({topic, subject}) {
     const data = useData((state) => state.data)
     const setLoading = useStore((state) => state.setLoading)
     const loading = useStore((state) => state.loading)
+    let [isOpen, setIsOpen] = useState(false)
+    const key = useStore((state) => state.key)
+    const [notes, setNotes] = useState(() => {
+        let x = {}
+        for (let i=0; i<data.notes.length; i++) {
+            x[data.notes[i].topic.id] = data.notes[i].content
+        }
+        return x
+    })
 
     function skill_level(id){
         const topics = data.skill_level.topics
@@ -81,23 +92,61 @@ function Topic({topic, subject}) {
 
     if (loading) return <FullPageLoader/>
 
+    function onChange(event) {
+        setNotes({
+            ...notes,
+            [topic.id]: event.target.value,
+        });
+    }
+
+    const notesField = (
+        <textarea className='notes-field' placeholder='Type here...' value={notes[topic.id]} onChange={onChange}></textarea>
+    )
+
+    async function saveNote() {
+        const noteContent = notes[topic.id]
+        
+        const response = await fetch(`${API_URL}/api/app/notes-save/`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Authorization: "Token " + key
+            },
+            body: JSON.stringify({"topic": topic.id, "content": notes[topic.id]}),
+        })
+    }
+
+    const noteSaveButton = (
+        <button
+            type="button"
+            className="inline-flex justify-center w-full rounded-md border border-transparent bg-secondery px-4 py-2 text-sm font-medium text-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+            onClick={() => {setIsOpen(false); saveNote();}}
+        >
+            Save
+        </button>
+    )
+
+
     return(
-        <div className='rows practice'>
-            <div className='left'>
-                <div className='topic-skill-level' style={{backgroundColor: `rgba(var(--${subject.toLowerCase()}-dark), 0.07)`}}>
-                    <Skill_level subject={subject.toLowerCase()} level={skill_level(topic.id)}/>
+        <>
+            <Popup isOpen={isOpen} setIsOpen={setIsOpen} title={"Notes"} content={notesField} button={noteSaveButton}/>
+            <div className='rows practice'>
+                <div className='left'>
+                    <div className='topic-skill-level' style={{backgroundColor: `rgba(var(--${subject.toLowerCase()}-dark), 0.07)`}}>
+                        <Skill_level subject={subject.toLowerCase()} level={skill_level(topic.id)}/>
+                    </div>
+                    <div className='topic-title'>
+                        {topic.title}
+                    </div>
                 </div>
-                <div className='topic-title'>
-                    {topic.title}
+                <div className='right'>
+                    <button className='notes' onClick={() => {setIsOpen(true); {!notes[topic.id] && setNotes({...notes, [topic.id]:""})}}}>
+                        <Notes_icon/>
+                        Notes
+                    </button>
+                    <button onClick={() => practiceExercise(topic.id, setLoading)} className="outlined practice-btn">Practice</button>
                 </div>
             </div>
-            <div className='right'>
-                <button className='notes'>
-                    <Notes_icon/>
-                    Notes
-                </button>
-                <button onClick={() => practiceExercise(topic.id, setLoading)} className="outlined practice-btn">Practice</button>
-            </div>
-        </div>
+        </>
     )
 }
