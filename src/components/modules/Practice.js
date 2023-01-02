57f4e5ceda6a03449ea41c4548dc68d94b675dbe
nahasco <alnahhas55@gmail.com>
2023-01-02 16:38:07 +0300

    manage 5 last days progress in the frontend

--- src/components/modules/Practice.js
+++ src/components/modules/Practice.js
@@ -22,6 +22,52 @@ export default function Practice(props) {
         {"title": "Topic 4", "id": "4"}
     ]
     
+    Date.prototype.withoutTime = function () {
+        var d = new Date(this);
+        d.setHours(0, 0, 0, 0);
+        return d;
+    }
+    
+    const today = new Date()
+
+    function minusDate(days) {
+        const x =  new Date()
+        const date = new Date(x.setDate(x.getDate() - days))
+        return date.withoutTime()
+    }
+
+
+    // for (let i=0; i<data.progress.last_5_days.length; i++) {
+    //     new Date(data.progress.last_5_days[i].date).withoutTime().getTime() == minusDate(5-i).getTime() ? console.log(true) : console.log(false)
+    //     console.log(minusDate(5-i))
+    //     console.log(new Date(data.progress.last_5_days[i].date).withoutTime())
+    // }
+
+    var todays_progress = null
+    const days = []
+
+    for (let h=1; h<6; h++) {
+        console.log(minusDate(6-h))
+        var found = null
+
+        for (let i=0; i<6; i++) {
+            if (new Date(data.progress.last_5_days[i].date).withoutTime().getTime() == minusDate(0).getTime()) {
+                todays_progress = data.progress.last_5_days[i]
+            }
+
+            if (minusDate(6-h).getTime() == new Date(data.progress.last_5_days[i].date).withoutTime().getTime()) {
+                days.push(data.progress.last_5_days[i])
+                found = true
+            }
+
+            if (i==5 && !found) {
+                days.push(null)
+            }
+        }
+    }
+    console.log(days)
+
+
     return (
         <div className="practice-top">
             <div className="widget recommended-practice">
@@ -55,14 +101,47 @@ export default function Practice(props) {
                 <div className="widget-content">
                     <div className="subject-today-progress">
                         <div className="subject-today-progress-title">Today</div>
-                        <ActivityRings total={data.progress.todays[`${props.subject.toLowerCase()}_progress`]} progress={(data.progress.todays[`${props.subject.toLowerCase()}_progress`]/data.progress.todays[`${props.subject.toLowerCase()}_total`])*100} subject={props.subject} height="150px" text={true}/>
+                        {
+                        todays_progress 
+                        ?
+                        <ActivityRings 
+                            total={todays_progress[`${props.subject.toLowerCase()}_progress`]} 
+                            progress={(todays_progress[`${props.subject.toLowerCase()}_progress`]/todays_progress[`${props.subject.toLowerCase()}_total`])*100} 
+                            subject={props.subject} 
+                            height="150px" 
+                            text={true}/>
+                        :
+                        <ActivityRings
+                            total={0} 
+                            progress={0} 
+                            subject={props.subject} 
+                            height="150px" 
+                            text={true}/>
+                        }
                     </div>
                     <div className="subject-last-progress">
                         <div className="subject-last-progress-title">Last 5 Days</div>
                         <div className="rings-last-7-days">
-                            {(data.progress.last_5_days).map(ring => {
-                                return <ActivityRings key={ring.id} total={ring[`${props.subject.toLowerCase()}_progress`]} progress={(ring[`${props.subject.toLowerCase()}_progress`]/ring[`${props.subject.toLowerCase()}_total`])*100} subject={props.subject} height="37px" text={true}/>
-                            })}
+                            {(days).map((ring, index) => (
+                                ring
+                                ?
+                                (<ActivityRings 
+                                key={ring.id} 
+                                total={ring[`${props.subject.toLowerCase()}_progress`]} 
+                                progress={(ring[`${props.subject.toLowerCase()}_progress`]/ring[`${props.subject.toLowerCase()}_total`])*100} 
+                                subject={props.subject} 
+                                height="37px" 
+                                text={true}/>)
+                                :
+                                (<ActivityRings
+                                key={`ring${index}`} 
+                                total={0} 
+                                progress={0} 
+                                subject={props.subject} 
+                                height="37px" 
+                                text={true}/>)
+                                
+                            ))}
                         </div>
                     </div>
                 </div>
